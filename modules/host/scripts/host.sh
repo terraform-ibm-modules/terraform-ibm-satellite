a1854db2591aed4fc097f48f4cee701b3a6284a8
Anil Kumar Nagaraj <anilkumar@in.ibm.com>
2021-01-13 15:35:12 +0530

    Add satellite module support for aws

--- modules/host/scripts/host.sh
+++ modules/host/scripts/host.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-​
 ibmcloud login --apikey=$API_KEY -a $ENDPOINT -r $REGION -g $RESOURCE_GROUP
 ibmcloud iam oauth-tokens
-​
 if [ "$PROVIDER" == "aws" ]; then
     hostname=$(echo $hostname | cut -d "." -f 1)
 fi
-​
 # Check host attached to location 
 host_out=`ibmcloud sat host ls --location $LOCATION | grep $hostname`
 rc=$?
 while [ $rc -eq 1 ]
 do
-if [[ $(ibmcloud sat host ls --location $LOCATION | grep $hostname) == 0 ]]; then
-    echo host $hostname attached
-    rc = 0
-  fi
+    host_out=`ibmcloud sat host ls --location $LOCATION | grep $hostname`
+    HOST_ID=$(echo $host_out| cut -d' ' -f 2)
+    if [[ $HOST_ID != "" ]]; then
+        echo host $hostname attached
+        rc=0
+        break
+    fi
     echo "************* hosts not attached to location *****************"
     sleep 10
 done    
-​
 HOST_ID=$(echo $host_out| cut -d' ' -f 2)
-
 if [[ $index == 0 ]]; then
     zone="$host_zone-1"
 elif [ $index == 1 ]; then
@@ -29,16 +27,12 @@ elif [ $index == 1 ]; then
 else
     zone="$host_zone-3"
 fi
-   
-   
 echo hostout= $host_out
 echo hostid= $HOST_ID
 echo hostname= $hostname
 echo location= $LOCATION
-
 # Assign host to location control plane
 ibmcloud sat host assign --cluster $LOCATION --location $LOCATION --host $HOST_ID --zone $zone
-
 status='notready'
 echo $status
 while [ "$status" != "ready" ]
@@ -46,11 +40,9 @@ do
    if [[ $(ibmcloud sat host ls --location $LOCATION | grep $hostname) == *"Ready"* ]]; then
     echo host $hostname ready
     status="ready"
+    break
   fi
     echo "************* hosts not ready *****************"
     sleep 10
 done
-
-
-
 # echo -n "{\"assign_output\":\"${asout}\"}"
--- modules/location/main.tf
+++ modules/location/main.tf
@@ -1,18 +1,39 @@
 
 resource "null_resource" "satellite_location" {
-  depends_on = [var.module_depends_on]
+
+  triggers = {
+      LOCATION       = var.location
+      API_KEY        = var.ibmcloud_api_key
+      REGION         = var.region
+      RESOURCE_GROUP = var.resource_group
+      ENDPOINT       = var.endpoint
+      PROVIDER       = var.host_provider
+  }
+
   provisioner "local-exec" {
-    command = ". ${path.module}/../../modules/scripts/location.sh"
+    when = create
+    command = ". ${path.module}/../../modules/location/scripts/location.sh"
     environment = {
-    ZONE = var.zone
-    LOCATION = var.location
-    LABEL = var.label
-    API_KEY = var.ibmcloud_api_key
-    REGION=var.region
-    RESOURCE_GROUP=var.resource_group
-    PROVIDER=var.host_provider
-    # COS_KEY = var.cos_key
-    # COS_KEY_ID = var.cos_key_id
+      ZONE           = var.zone
+      LOCATION       = var.location
+      LABEL          = var.label
+      API_KEY        = var.ibmcloud_api_key
+      REGION         = var.region
+      RESOURCE_GROUP = var.resource_group
+      PROVIDER       = var.host_provider
+      ENDPOINT       = var.endpoint
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = ". ${path.module}/../../modules/location/scripts/destroy.sh"
+    environment = {
+      LOCATION       = self.triggers.LOCATION
+      API_KEY        = self.triggers.API_KEY
+      REGION         = self.triggers.REGION
+      RESOURCE_GROUP = self.triggers.RESOURCE_GROUP
+      ENDPOINT       = self.triggers.ENDPOINT
     }
   }
 }
