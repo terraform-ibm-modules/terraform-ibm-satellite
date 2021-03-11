b37ada2bc3a2941c0e6a9d91532407c1da61ff94
zachary-gleason <80419304+zachary-gleason@users.noreply.github.com>
2021-03-11 16:22:06 -0500

    fix(host): Fix host script

--- modules/host/scripts/destroy.sh
+++ modules/host/scripts/destroy.sh
@@ -4,6 +4,6 @@ echo "************* Deleting Hosts *****************"
 
 loc_id=$(ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION | awk '{print $2}')
 hostname=$(echo $hostname | cut -d'.' -f1)
-host_id=$(ic sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
+host_id=$(ibmcloud sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
 ibmcloud login --apikey=$API_KEY -a $ENDPOINT -r $REGION -g $RESOURCE_GROUP
 ibmcloud sat host rm --location $loc_id --host $host_id -f
