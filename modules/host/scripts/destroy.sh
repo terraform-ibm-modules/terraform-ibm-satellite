d7579e283e8f0a5b040da679248b51cda6c9dc38
Zachary Gleason <zachary.gleason@ibm.com>
2021-03-11 17:24:54 -0500

    test(host): Test new destroy_hosts

--- modules/host/scripts/destroy.sh
+++ modules/host/scripts/destroy.sh
@@ -5,5 +5,6 @@ echo "************* Deleting Hosts *****************"
 loc_id=$(ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION | awk '{print $2}')
 hostname=$(echo $hostname | cut -d'.' -f1)
 host_id=$(ibmcloud sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
+echo "removing host $host_id #########"
 ibmcloud login --apikey=$API_KEY -a $ENDPOINT -r $REGION -g $RESOURCE_GROUP
 ibmcloud sat host rm --location $loc_id --host $host_id -f
