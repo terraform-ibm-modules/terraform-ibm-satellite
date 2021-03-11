bae64806e451088fe4e7ca947326be0209fcf3f1
zachary-gleason <80419304+zachary-gleason@users.noreply.github.com>
2021-03-11 17:43:18 -0500

    test(host): Test host destroy fix

--- modules/host/scripts/destroy.sh
+++ modules/host/scripts/destroy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "************* Deleting Hosts *****************"
-
+ibmcloud plugin list
 loc_id=$(ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION | awk '{print $2}')
 hostname=$(echo $hostname | cut -d'.' -f1)
 host_id=$(ibmcloud sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
