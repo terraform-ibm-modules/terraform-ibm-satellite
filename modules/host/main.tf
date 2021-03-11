d91e87b09c725f6bcc763a9d41428db901b9c1ef
Zachary Gleason <zachary.gleason@ibm.com>
2021-03-11 18:00:13 -0500

    test(host): test if host destroy works

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -23,16 +23,29 @@ resource "null_resource" "assign_host" {
     command = ". ${path.module}/scripts/host.sh"
 
     environment = {
-      hostname       = element(var.host_vms, count.index)
+      hostname       = element(split(",", self.triggers.host_vms_joined), count.index)
       index          = count.index
-      LOCATION       = var.location_name
-      API_KEY        = var.ibmcloud_api_key
-      REGION         = var.ibm_region
-      RESOURCE_GROUP = var.resource_group
-      ENDPOINT       = var.endpoint
-      PROVIDER       = var.host_provider
+      LOCATION       = self.triggers.LOCATION
+      API_KEY        = self.triggers.API_KEY
+      REGION         = self.triggers.REGION
+      RESOURCE_GROUP = self.triggers.RESOURCE_GROUP
+      ENDPOINT       = self.triggers.ENDPOINT
+      PROVIDER       = self.triggers.PROVIDER
     }
   }
 
+  provisioner "local-exec" {
+    when    = destroy
+    command = ". ${path.module}/scripts/destroy.sh"
+
+    environment = {
+      hostname       = element(split(",", self.triggers.host_vms_joined), count.index)
+      LOCATION       = self.triggers.LOCATION
+      API_KEY        = self.triggers.API_KEY
+      REGION         = self.triggers.REGION
+      RESOURCE_GROUP = self.triggers.RESOURCE_GROUP
+      ENDPOINT       = self.triggers.ENDPOINT
+    }
+  }
 }
 
--- modules/host/scripts/destroy.sh
+++ modules/host/scripts/destroy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
 
 echo "************* Deleting Hosts *****************"
-echo "location $LOCATION"
-ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION
-ibmcloud plugin list
 loc_id=$(ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION | awk '{print $2}')
 hostname=$(echo $hostname | cut -d'.' -f1)
 host_id=$(ibmcloud sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
+echo "location $LOCATION"
 echo "loc_id $loc_id"
 echo "hostname $hostname"
 echo "removing host $host_id #########"
