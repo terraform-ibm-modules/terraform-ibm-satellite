83eeca13a75daafae6ff4cc767ba3c9cd09a3603
zachary-gleason <80419304+zachary-gleason@users.noreply.github.com>
2021-03-11 19:22:48 -0500

    test(host): Login to ibmcloud earlier

--- modules/host/scripts/destroy.sh
+++ modules/host/scripts/destroy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 echo "************* Deleting Hosts *****************"
+ibmcloud login --apikey=$API_KEY -a $ENDPOINT -r $REGION -g $RESOURCE_GROUP
 loc_id=$(ibmcloud sat location ls 2>&1 | grep -m 1 $LOCATION | awk '{print $2}')
 hostname=$(echo $hostname | cut -d'.' -f1)
 host_id=$(ibmcloud sat host ls --location $loc_id | grep $hostname | tr -s ' ' | cut -d' ' -f2)
@@ -8,5 +9,4 @@ echo "location $LOCATION"
 echo "loc_id $loc_id"
 echo "hostname $hostname"
 echo "removing host $host_id #########"
-ibmcloud login --apikey=$API_KEY -a $ENDPOINT -r $REGION -g $RESOURCE_GROUP
 ibmcloud sat host rm --location $loc_id --host $host_id -f
