c03181f524c2d5716b84271313571ef51426ba88
kavya498 <hkavya26@in.ibm.com>
2021-05-25 18:26:33 +0530

    Support: Add data disk to azure instance

--- examples/satellite-azure/host.tf
+++ examples/satellite-azure/host.tf
@@ -1,6 +1,6 @@
 module "satellite-host" {
-  source = "../../modules/host"
-
+  depends_on     = [azurerm_virtual_machine_data_disk_attachment.disk_attach]
+  source         = "../../modules/host"
   host_count     = var.satellite_host_count
   location       = module.satellite-location.location_id
   host_vms       = azurerm_linux_virtual_machine.az_host.*.name
