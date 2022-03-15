62c3513aa5f01c2941e95f96a34e782f5b508cb1
Clayton Huff <crhuff@ibm.com>
2022-03-15 14:24:50 -0500

    change data structure for aws hosts to allow multi host type

--- examples/satellite-aws/host.tf
+++ examples/satellite-aws/host.tf
@@ -7,10 +7,12 @@ module "satellite-host" {
   //Uncomment following line to point the source to registry level module
   //source = "terraform-ibm-modules/satellite/ibm//modules/host"
 
+  for_each = var.hosts
+
   source         = "../../modules/host"
-  host_count     = var.satellite_host_count
+  host_count     = each.value.for_control_plane ? each.value.count : 0
   location       = module.satellite-location.location_id
-  host_vms       = module.ec2.private_dns
+  host_vms       = module.ec2[each.key].private_dns
   location_zones = length(var.location_zones) == 0 ? local.azs : var.location_zones
   host_labels    = var.host_labels
   host_provider  = "aws"
