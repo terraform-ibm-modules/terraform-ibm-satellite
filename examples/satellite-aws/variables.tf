783b66c9153d19b002dd2c36e911aedb396c950b
Clayton Huff <crhuff@ibm.com>
2022-03-17 09:44:14 -0500

    backwards compatibility

--- examples/satellite-aws/variables.tf
+++ examples/satellite-aws/variables.tf
@@ -96,6 +96,29 @@ variable "host_provider" {
 ##################################################
 # AWS EC2 Variables
 ##################################################
+variable "satellite_host_count" {
+  description = "The total number of AWS host to create for control plane. satellite_host_count value should always be in multiples of 3, such as 3, 6, 9, or 12 hosts"
+  type        = number
+  nullable    = true
+  default     = null
+  validation {
+    condition     = var.satellite_host_count == null || ((can((var.satellite_host_count % 3) == 0)) && can(var.satellite_host_count > 0))
+    error_message = "Sorry, host_count value should always be in multiples of 3, such as 6, 9, or 12 hosts."
+  }
+}
+variable "addl_host_count" {
+  description = "The total number of additional aws host"
+  type        = number
+  nullable    = true
+  default     = null
+}
+
+variable "instance_type" {
+  description = "The type of aws instance to create"
+  type        = string
+  nullable    = true
+  default     = null
+}
 variable "cp_hosts" {
   description = "A map of AWS host objects used to create the location control plane, including instance_type and count. Control plane count value should always be in multiples of 3, such as 3, 6, 9, or 12 hosts."
   type = list(
