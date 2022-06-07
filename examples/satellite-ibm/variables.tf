fbd71be1fc3c2b2e7e81be1fd7e151e7d3843d22
Lionel Mace <lionel.mace@gmail.com>
2022-06-07 20:09:14 +0200

    revert back

--- examples/satellite-ibm/variables.tf
+++ examples/satellite-ibm/variables.tf
@@ -108,7 +108,7 @@ variable "cluster_profile" {
 ##################################################
 
 variable "create_cluster" {
-  description = "Create Cluster: Disable this, not to provision cluster"
+  description = "Create Cluster: Disable this, not to provision a cluster"
   type        = bool
   default     = false
 }
